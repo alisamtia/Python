75ec2ecfa226a6fa805685e838d2a053061e4529
Muhammad Ali Samtia <contact@alisamtia.com>
2023-11-27 15:07:11 +0500

    Add files via upload

---
+++ My Base of Python/Chapter 0/Terminal commands.py
@@ -0,0 +1,13 @@
+# pwd   : pwd to check your correct location
+# ls    : take the list of all files that are in the folder
+# clear : clear the terminal
+# cd    : change location
+# cd .. : Go back
+# mkdir : Create Folder
+# Touch : Create a new file
+# rm "f": Remove a file
+# rm -rf: remove a folder
+# Python: Run a file
+# code  : Read file in visual studio
+# mv o n: Rename A file
+# mv    : move a file
